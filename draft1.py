ad10c238a49ba44d60824fa7f167b2ef9086a67b
David Schonfeld <davidaschonfeld@gmail.com>
2025-01-30 13:15:42 -0500

    Adding xmltodict so no matter if I receive JSON or XML data, I get the same identical data (as long as the website provides me with the same data no matter what format they send it to me)

--- draft1.py
+++ draft1.py
@@ -1,5 +1,5 @@
 import os
-from typing import Union
+from typing import Any, Dict
 
 import pandas as pd
 from pandas import DataFrame
@@ -26,7 +26,7 @@ class xmlOrJson(Enum):
     JSON = 1
 
 
-def sendRequest_toITIS(endStringRequest : str, responseContentFormat : xmlOrJson) -> Union[bytes, None]:
+def sendRequest_toITIS(endStringRequest : str, responseContentFormat : xmlOrJson) -> Dict[Any, Any]:
     requestToSend_full : str
     if (responseContentFormat == xmlOrJson.XML):
         requestToSend_full : str = os.path.join(itis_url_beginning, itis_receiveFormat.XML.value, endStringRequest)
@@ -35,37 +35,42 @@ def sendRequest_toITIS(endStringRequest : str, responseContentFormat : xmlOrJson
     else:
         raise TypeError("responseContentFormat should be \"XML\" or \"JSON\" but it is "+str(responseContentFormat))
     
-    response : requests.Response = requests.get(requestToSend_full)
-    if (response.status_code >= 400):
+    try:
+        response : requests.Response = requests.get(requestToSend_full)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
         print("response.status_code: "+str(response.status_code))
-        return
-    elif (response.status_code == 200):
-        ## All good
+    except Exception as error:
+        pass
 
-        #TODO: Finish this method
-        print("response.content:")
-        print(str(response.content))
-        print("---------")
-        if (responseContentFormat == xmlOrJson.XML):
+    print("response.content:")
+    print(str(response.content))
+    print("---------")
+    responseContent_dict : dict
+    if (responseContentFormat == xmlOrJson.XML):
 
-            root : etree._Element = etree.XML(response.content, etree.XMLParser())
-            print(type(root))
-            print(etree.tostring(root))
+        root : etree._Element = etree.XML(response.content, etree.XMLParser())
+        print(type(root))
+        print(etree.tostring(root))
+        print("---")
 
-            testDict : dict = xmltodict.parse(response.content)
-            print(testDict)
-        elif (responseContentFormat == xmlOrJson.JSON):
-            print("TODO: JSON stuff")
-            jsonObject : dict = json.loads(response.content)
-            print(jsonObject)
+        responseContent_dict : dict = xmltodict.parse(response.content)
+    elif (responseContentFormat == xmlOrJson.JSON):
+        print("TODO: JSON stuff")
+        responseContent_dict : dict = json.loads(response.content)
+    print("-------------")
 
-    else:
-        raise NotImplementedError("Code that responds to Response Codes that are <400 and NOT 200 are not implemented yet.")
-
-
-        # return response._content
-    
+    return responseContent_dict
 
 # "http://www.itis.gov/ITISWebService/services/ITISService/searchForAnyMatchPaged?srchKey=dolphin&pageSize=2&pageNum=1&ascend=false"
 testRequest_request : str = "searchForAnyMatchPaged?srchKey=dolphin&pageSize=2&pageNum=1&ascend=false"
-sendRequest_toITIS(testRequest_request, xmlOrJson.JSON)
+dataReceived_request1_json : dict = sendRequest_toITIS(testRequest_request, xmlOrJson.JSON)
+dataReceived_request1_xml : dict = sendRequest_toITIS(testRequest_request, xmlOrJson.XML)
+if (dataReceived_request1_json == dataReceived_request1_xml):
+    print("Results from JSON and XML are the same")
+else:
+    print("Results from JSON and XML are the same")
+print("------dataReceived--JSON---")
+print(dataReceived_request1_json)
+print("------dataReceived--XML----")
+print(dataReceived_request1_xml)
