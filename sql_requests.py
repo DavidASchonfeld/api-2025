3ac9b92cff9a4b1e92eb6f70f0626d356f988c90
David Schonfeld <davidaschonfeld@gmail.com>
2025-02-03 14:30:47 -0500

    Adding obtaining results from a SQL command and notes about it.

--- sql_requests.py
+++ sql_requests.py
@@ -6,6 +6,9 @@ print(str(sqlalchemy.__version__))
 engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
 # "How do we locate the database? In this case, our URL includes the phrase /:memory:, which is an indicator to the sqlite3 module that we will be using an in-memory-only database. This kind of database is perfect for experimenting as it does not require any server nor does it need to create new files." (Description from: https://docs.sqlalchemy.org/en/20/tutorial/engine.html)
 
+# Going through SQLAlchemy Docs.
+# I've used PyoDBC more than SQLAlchemy, so I"m familiarizing with SQLAlchemy
+
 with engine.connect() as connection:
     result = connection.execute(text("Select 'Hello World'"))
     print(result.all())
@@ -16,6 +19,10 @@ with engine.connect() as connection:
              [{"x": 1, "y": 1}, {"x" : 2, "y" : 2}]
     )
     connection.commit()
+
+    result = connection.execute(text("SELECT x, y FROM test_one"))
+    for eachRow in result:
+        print(f"x: {eachRow.x} y: {eachRow.y}")
 
 
 
