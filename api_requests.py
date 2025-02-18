76c2f8e6a6efb5ab09e63433c7887fddefce30f3
David Schonfeld <davidaschonfeld@gmail.com>
2025-02-18 11:17:00 -0500

    Added new method for Smithsonian's API, and moved my api tests to a separate file

--- api_requests.py
+++ api_requests.py
@@ -14,9 +14,16 @@ import xmltodict
 
 # ITIS stands for "Integrated Taxonomic Information System"
 
-import constants
+from constants import api_keys
 
-itis_url_beginning : str = "http://www.itis.gov/ITISWebService/"
+api_url_beginnings : dict = {
+    "itis" : "http://www.itis.gov/ITISWebService/",
+    "smithsonian" : "https://api.si.edu/openaccess/api/v1.0"
+}
+# itis_url_beginning : str = "http://www.itis.gov/ITISWebService/"
+# smithsonian_url_beginning : str = "https://api.si.edu/openaccess/api/v1.0"
+
+
 class itis_receiveFormat(Enum):
     XML = "services/ITISService"
     JSON = "jsonservice"
@@ -29,9 +36,9 @@ class xmlOrJson(Enum):
 def sendRequest_toITIS(endStringRequest : str, responseContentFormat : xmlOrJson) -> Dict[Any, Any]:
     requestToSend_full : str
     if (responseContentFormat == xmlOrJson.XML):
-        requestToSend_full : str = os.path.join(itis_url_beginning, itis_receiveFormat.XML.value, endStringRequest)
+        requestToSend_full : str = os.path.join(api_url_beginnings["itis"], itis_receiveFormat.XML.value, endStringRequest)
     elif (responseContentFormat == xmlOrJson.JSON):
-        requestToSend_full : str = os.path.join(itis_url_beginning, itis_receiveFormat.JSON.value, endStringRequest)
+        requestToSend_full : str = os.path.join(api_url_beginnings["itis"], itis_receiveFormat.JSON.value, endStringRequest)
     else:
         raise TypeError("responseContentFormat should be \"XML\" or \"JSON\" but it is "+str(responseContentFormat))
     
@@ -58,16 +65,23 @@ def sendRequest_toITIS(endStringRequest : str, responseContentFormat : xmlOrJson
 
 
 
-###### Test ######
+def sendRequest_toSmithsonian(endStringRequest : str, inParams : dict) -> Dict[Any, Any]:
+    
+    requestToSend_full : str = os.path.join(api_url_beginnings["smithsonian"], endStringRequest)
 
-# "http://www.itis.gov/ITISWebService/services/ITISService/searchForAnyMatchPaged?srchKey=dolphin&pageSize=2&pageNum=1&ascend=false"
-testRequest_request : str = "searchForAnyMatchPaged?srchKey=dolphin&pageSize=2&pageNum=1&ascend=false"
-dataReceived_request1_json : dict = sendRequest_toITIS(testRequest_request, xmlOrJson.JSON)
-dataReceived_request1_xml : dict = sendRequest_toITIS(testRequest_request, xmlOrJson.XML)
+    inParams["api_key"] = api_keys["Data.Gov"]
 
-if (dataReceived_request1_json == dataReceived_request1_xml):
-    print("Results from JSON and XML are the same")
-    # They are the same!
-else:
-    print("Results from JSON and XML are the same")
-##################
+    try:
+        response : requests.Response = requests.get(requestToSend_full, params = inParams)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
+        print("response.status_code: "+str(response.status_code))
+        raise requests.exceptions.HTTPError("Response Status Code: "+str(response.status_code))
+    except Exception as error:
+        pass
+
+    ## JSON Response
+    responseContent_dict : dict = json.loads(response.content)
+
+    return responseContent_dict
+
---
+++ tests_api_requests.py
@@ -0,0 +1,29 @@
+from api_requests import sendRequest_toITIS
+from api_requests import sendRequest_toSmithsonian
+
+from api_requests import xmlOrJson
+
+
+# ###### Test Smithsonian #####
+# testRequest_endStringRequest : str = "search"
+# testRequest_params : dict = {
+#     "q" : "gorilla"
+# }
+# dataReceived_request1_json : dict = sendRequest_toSmithsonian(testRequest_endStringRequest, testRequest_params)
+# print(dataReceived_request1_json)
+
+
+###### Test ITIS ######
+
+# "http://www.itis.gov/ITISWebService/services/ITISService/searchForAnyMatchPaged?srchKey=dolphin&pageSize=2&pageNum=1&ascend=false"
+testRequest_request : str = "searchForAnyMatchPaged?srchKey=dolphin&pageSize=2&pageNum=1&ascend=false"
+dataReceived_request1_json : dict = sendRequest_toITIS(testRequest_request, xmlOrJson.JSON)
+dataReceived_request1_xml : dict = sendRequest_toITIS(testRequest_request, xmlOrJson.XML)
+
+if (dataReceived_request1_json == dataReceived_request1_xml):
+    print("Results from JSON and XML are the same")
+    # They are the same!
+else:
+    print("Results from JSON and XML are the same")
+print(dataReceived_request1_json)
+##################
