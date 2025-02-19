7f9dc94b5a4f6dc68d65c42ccb43f12b51929efd
David Schonfeld <davidaschonfeld@gmail.com>
2025-02-19 14:34:15 -0500

    Rearranged api_requests.py so each api has its own class

--- api_requests.py
+++ api_requests.py
@@ -16,73 +16,175 @@ import xmltodict
 
 from constants import api_keys
 
-api_url_beginnings : dict = {
-    "itis" : "http://www.itis.gov/ITISWebService/",
-    "smithsonian" : "https://api.si.edu/openaccess/api/v1.0"
-}
-
 class xmlOrJson(Enum):
-    XML = 0
-    JSON = 1
-
-class itis_receiveFormat(Enum):
-    XML = "services/ITISService"
-    JSON = "jsonservice"
+        XML = 0
+        JSON = 1
 
 
-# ITIS API References:
-# -- https://www.itis.gov/ws_searchApiDescription.html
-# -- https://www.itis.gov/ws_description.html 
 
-def sendRequest_toITIS(endStringRequest : str, responseContentFormat : xmlOrJson) -> Dict[Any, Any]:
-    requestToSend_full : str
-    if (responseContentFormat == xmlOrJson.XML):
-        requestToSend_full : str = os.path.join(api_url_beginnings["itis"], itis_receiveFormat.XML.value, endStringRequest)
-    elif (responseContentFormat == xmlOrJson.JSON):
-        requestToSend_full : str = os.path.join(api_url_beginnings["itis"], itis_receiveFormat.JSON.value, endStringRequest)
-    else:
-        raise TypeError("responseContentFormat should be \"XML\" or \"JSON\" but it is "+str(responseContentFormat))
-    
-    try:
-        response : requests.Response = requests.get(requestToSend_full)
-        response.raise_for_status()
-    except requests.exceptions.HTTPError:
-        print("response.status_code: "+str(response.status_code))
-        raise requests.exceptions.HTTPError("Response Status Code: "+str(response.status_code))
-    except Exception as error:
+
+class apiRequestsLibrary:
+    requestLimitPerHour : int
+    guideReferences : list[str]
+    api_url_beginning : str
+
+
+    def __init__(self):
+        pass
+    def sendRequest(self):
         pass
 
-    responseContent_dict : dict
-    if (responseContentFormat == xmlOrJson.XML):
+class ITIS_requests(apiRequestsLibrary):
+    requestLimitPerHour = 0
+    guideReferences = [
+        "https://www.itis.gov/ws_searchApiDescription.html", 
+        "https://www.itis.gov/ws_description.html"
+    ]
+    api_url_beginning : str = "http://www.itis.gov/ITISWebService/"
 
-        root : etree._Element = etree.XML(response.content, etree.XMLParser())
-        # print(etree.tostring(root))
+    class itis_receiveFormat(Enum):
+        XML = "services/ITISService"
+        JSON = "jsonservice"
 
-        responseContent_dict : dict = xmltodict.parse(response.content)
-    elif (responseContentFormat == xmlOrJson.JSON):
+
+    @classmethod
+    def sendRequest(cls : type, endStringRequest : str, responseContentFormat : xmlOrJson) -> Dict[Any, Any]:
+        # requestToSend_full : str
+        if (responseContentFormat == xmlOrJson.XML):
+            requestToSend_full : str = os.path.join(cls.api_url_beginning, cls.itis_receiveFormat.XML.value, endStringRequest)
+        elif (responseContentFormat == xmlOrJson.JSON):
+            requestToSend_full : str = os.path.join(cls.api_url_beginning, cls.itis_receiveFormat.JSON.value, endStringRequest)
+        else:
+            raise TypeError("responseContentFormat should be \"XML\" or \"JSON\" but it is "+str(responseContentFormat))
+        
+        try:
+            response : requests.Response = requests.get(requestToSend_full)
+            response.raise_for_status()
+        except requests.exceptions.HTTPError:
+            print("response.status_code: "+str(response.status_code))
+            raise requests.exceptions.HTTPError("Response Status Code: "+str(response.status_code))
+        except Exception as error:
+            pass
+
+        responseContent_dict : dict
+        if (responseContentFormat == xmlOrJson.XML):
+
+            root : etree._Element = etree.XML(response.content, etree.XMLParser())
+            # print(etree.tostring(root))
+
+            responseContent_dict : dict = xmltodict.parse(response.content)
+        elif (responseContentFormat == xmlOrJson.JSON):
+            responseContent_dict : dict = json.loads(response.content)
+
+        return responseContent_dict
+
+
+class Smithsonian_requests(apiRequestsLibrary):
+    requestLimitPerHour = 1000
+    guideReferences = [
+        "https://edan.si.edu/openaccess/apidocs/"
+    ]
+    api_url_beginning : str = "https://api.si.edu/openaccess/api/v1.0"
+
+    @classmethod
+    def sendRequest(cls : type, endStringRequest : str, inParams : dict) -> Dict[Any, Any]:
+        
+        requestToSend_full : str = os.path.join(cls.api_url_beginning, endStringRequest)
+
+        inParams["api_key"] = api_keys["Data.Gov"]
+
+        try:
+            response : requests.Response = requests.get(requestToSend_full, params = inParams)
+            response.raise_for_status()
+        except requests.exceptions.HTTPError:
+            print("response.status_code: "+str(response.status_code))
+            raise requests.exceptions.HTTPError("Response Status Code: "+str(response.status_code))
+        except Exception as error:
+            pass
+
+        ## JSON Response
         responseContent_dict : dict = json.loads(response.content)
 
-    return responseContent_dict
+        return responseContent_dict
 
 
-# Smithsonian API Reference: https://edan.si.edu/openaccess/apidocs/
-def sendRequest_toSmithsonian(endStringRequest : str, inParams : dict) -> Dict[Any, Any]:
+
+
+
+
+
+
+
+
+
+
+# ## Original Methods:
+
+
+# class itis_receiveFormat(Enum):
+#     XML = "services/ITISService"
+#     JSON = "jsonservice"
+
+# api_url_beginnings : dict = {
+#     "itis" : "http://www.itis.gov/ITISWebService/",
+#     "smithsonian" : "https://api.si.edu/openaccess/api/v1.0"
+# }
+
+# # ITIS API References:
+# # -- https://www.itis.gov/ws_searchApiDescription.html
+# # -- https://www.itis.gov/ws_description.html 
+
+
+
+# def sendRequest_toITIS(endStringRequest : str, responseContentFormat : xmlOrJson) -> Dict[Any, Any]:
+#     requestToSend_full : str
+#     if (responseContentFormat == xmlOrJson.XML):
+#         requestToSend_full : str = os.path.join(api_url_beginnings["itis"], itis_receiveFormat.XML.value, endStringRequest)
+#     elif (responseContentFormat == xmlOrJson.JSON):
+#         requestToSend_full : str = os.path.join(api_url_beginnings["itis"], itis_receiveFormat.JSON.value, endStringRequest)
+#     else:
+#         raise TypeError("responseContentFormat should be \"XML\" or \"JSON\" but it is "+str(responseContentFormat))
     
-    requestToSend_full : str = os.path.join(api_url_beginnings["smithsonian"], endStringRequest)
+#     try:
+#         response : requests.Response = requests.get(requestToSend_full)
+#         response.raise_for_status()
+#     except requests.exceptions.HTTPError:
+#         print("response.status_code: "+str(response.status_code))
+#         raise requests.exceptions.HTTPError("Response Status Code: "+str(response.status_code))
+#     except Exception as error:
+#         pass
 
-    inParams["api_key"] = api_keys["Data.Gov"]
+#     responseContent_dict : dict
+#     if (responseContentFormat == xmlOrJson.XML):
 
-    try:
-        response : requests.Response = requests.get(requestToSend_full, params = inParams)
-        response.raise_for_status()
-    except requests.exceptions.HTTPError:
-        print("response.status_code: "+str(response.status_code))
-        raise requests.exceptions.HTTPError("Response Status Code: "+str(response.status_code))
-    except Exception as error:
-        pass
+#         root : etree._Element = etree.XML(response.content, etree.XMLParser())
+#         # print(etree.tostring(root))
 
-    ## JSON Response
-    responseContent_dict : dict = json.loads(response.content)
+#         responseContent_dict : dict = xmltodict.parse(response.content)
+#     elif (responseContentFormat == xmlOrJson.JSON):
+#         responseContent_dict : dict = json.loads(response.content)
 
-    return responseContent_dict
+#     return responseContent_dict
+
+
+# # Smithsonian API Reference: https://edan.si.edu/openaccess/apidocs/
+# def sendRequest_toSmithsonian(endStringRequest : str, inParams : dict) -> Dict[Any, Any]:
+    
+#     requestToSend_full : str = os.path.join(api_url_beginnings["smithsonian"], endStringRequest)
+
+#     inParams["api_key"] = api_keys["Data.Gov"]
+
+#     try:
+#         response : requests.Response = requests.get(requestToSend_full, params = inParams)
+#         response.raise_for_status()
+#     except requests.exceptions.HTTPError:
+#         print("response.status_code: "+str(response.status_code))
+#         raise requests.exceptions.HTTPError("Response Status Code: "+str(response.status_code))
+#     except Exception as error:
+#         pass
+
+#     ## JSON Response
+#     responseContent_dict : dict = json.loads(response.content)
+
+#     return responseContent_dict
 
--- tests_api_requests.py
+++ tests_api_requests.py
@@ -1,16 +1,9 @@
-from api_requests import sendRequest_toITIS
-from api_requests import sendRequest_toSmithsonian
-
-from api_requests import xmlOrJson
-
+#External Libraries
 from pprint import pprint
 
+# My Created Libraries
+from api_requests import xmlOrJson
 from outputTextWriter import OutputTextWriter
-
-
-
-
-
 
 writer : OutputTextWriter = OutputTextWriter()
 
@@ -23,7 +16,9 @@ testRequest_endStringRequest : str = "search"
 testRequest_params : dict = {
     "q" : "gorilla"
 }
-dataReceived_request1_json : dict = sendRequest_toSmithsonian(testRequest_endStringRequest, testRequest_params)
+from api_requests import Smithsonian_requests
+dataReceived_request1_json : dict = Smithsonian_requests.sendRequest(testRequest_endStringRequest, testRequest_params)
+
 
 writer.print("Test1")
 writer.print("Test2")
@@ -32,22 +27,26 @@ writer.print_dict(dataReceived_request1_json, prettyPrint= True)
 
 writer.print("Test1")
 writer.print("Test2")
+writer.print("\n--------------------------\n")
 
 
-# writer.print_dict(dataReceived_request1_json, True)
 
+##### Test ITIS ######
 
-###### Test ITIS ######
+"http://www.itis.gov/ITISWebService/services/ITISService/searchForAnyMatchPaged?srchKey=dolphin&pageSize=2&pageNum=1&ascend=false"
+testRequest_request : str = "searchForAnyMatchPaged?srchKey=dolphin&pageSize=2&pageNum=1&ascend=false"
 
-# "http://www.itis.gov/ITISWebService/services/ITISService/searchForAnyMatchPaged?srchKey=dolphin&pageSize=2&pageNum=1&ascend=false"
-# testRequest_request : str = "searchForAnyMatchPaged?srchKey=dolphin&pageSize=2&pageNum=1&ascend=false"
-# dataReceived_request1_json : dict = sendRequest_toITIS(testRequest_request, xmlOrJson.JSON)
-# dataReceived_request1_xml : dict = sendRequest_toITIS(testRequest_request, xmlOrJson.XML)
+from api_requests import ITIS_requests
+dataReceived_request1_json : dict = ITIS_requests.sendRequest(testRequest_request, xmlOrJson.JSON)
+dataReceived_request1_xml : dict = ITIS_requests.sendRequest(testRequest_request, xmlOrJson.XML)
 
-# if (dataReceived_request1_json == dataReceived_request1_xml):
-#     print("Results from JSON and XML are the same")
-#     # They are the same!
-# else:
-#     print("Results from JSON and XML are the same")
-# print(dataReceived_request1_json)
-# ##################
+if (dataReceived_request1_json == dataReceived_request1_xml):
+    print("Results from JSON and XML are the same")
+    # They are the same!
+else:
+    print("Results from JSON and XML are the same")
+
+writer.print_dict(dataReceived_request1_json, prettyPrint= True)
+writer.print("test3")
+
+##################
