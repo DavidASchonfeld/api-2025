00894090e88b01c3fd0e51e259b4a152706756bd
David Schonfeld <davidaschonfeld@gmail.com>
2025-02-19 15:26:40 -0500

    Testing pandas.merge (aka pd.merge) in tests_api_requests.py

--- tests_api_requests.py
+++ tests_api_requests.py
@@ -1,5 +1,7 @@
 #External Libraries
 from pprint import pprint
+import pandas as pd
+from pandas import DataFrame
 
 # My Created Libraries
 from api_requests import xmlOrJson
@@ -29,12 +31,15 @@ writer.print("Test1")
 writer.print("Test2")
 writer.print("\n--------------------------\n")
 
+smithsonian_gorilla : DataFrame = DataFrame(dataReceived_request1_json)
+
 
 
 ##### Test ITIS ######
 
-"http://www.itis.gov/ITISWebService/services/ITISService/searchForAnyMatchPaged?srchKey=dolphin&pageSize=2&pageNum=1&ascend=false"
-testRequest_request : str = "searchForAnyMatchPaged?srchKey=dolphin&pageSize=2&pageNum=1&ascend=false"
+# "http://www.itis.gov/ITISWebService/services/ITISService/searchForAnyMatchPaged?srchKey=dolphin&pageSize=2&pageNum=1&ascend=false"
+# testRequest_request : str = "searchForAnyMatchPaged?srchKey=dolphin&pageSize=2&pageNum=1&ascend=false"
+testRequest_request : str = "searchForAnyMatchPaged?srchKey=gorilla&pageSize=2&pageNum=1&ascend=false"
 
 from api_requests import ITIS_requests
 dataReceived_request1_json : dict = ITIS_requests.sendRequest(testRequest_request, xmlOrJson.JSON)
@@ -49,4 +54,13 @@ else:
 writer.print_dict(dataReceived_request1_json, prettyPrint= True)
 writer.print("test3")
 
+itis_gorilla : DataFrame = DataFrame(dataReceived_request1_json)
+
 ##################
+
+mergeOne : DataFrame = pd.merge(smithsonian_gorilla, itis_gorilla, left_on = 'title_sort', right_on = 'name')
+
+writer.print("\n--------------------\n")
+
+print(mergeOne)
+
